d350f3536fb1bd4f34615df7319fedd6ebd30d20
Taki <tk@noubies.com>
2020-05-04 18:53:21 +0900

    ✨ Publishing your site
    https://docusaurus.io/docs/en/publishing#using-github-pages

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -23,14 +23,14 @@ const users = [
 const siteConfig = {
   title: 'baby-degu', // Title for your website.
   tagline: "Let's seek out what we can do",
-  url: 'https://qiita.com/baby-degu', // Your website URL
-  baseUrl: '/', // Base URL for your project */
+  url: 'https://baby-degu.github.io', // Your website URL
+  baseUrl: '/docusaurus/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'test-site',
+  projectName: 'docusaurus',
   organizationName: 'baby-degu',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
