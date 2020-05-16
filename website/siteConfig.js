b005f9a23d409930ed90e6a2c146fd4d4c4974fc
Taki <tk@noubies.com>
2020-05-16 12:51:29 +0900

    :sparkles: Add github link

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -42,6 +42,7 @@ const siteConfig = {
 		{ doc: 'doc1', label: 'Docs' },
 		{ doc: 'doc4', label: 'API' },
 		{ page: 'help', label: 'Help' },
+		{ href: 'https://github.com/baby-degu/docusaurus', label: 'Github', external: true },
 		{ blog: true, label: 'Blog' },
 	],
 
