683d61ab586e97772a8a03f90c2783bb412ba54d
Taki <tk@noubies.com>
2020-06-09 09:15:57 +0900

    :wrench: Fix projectName

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -19,7 +19,7 @@ const siteConfig = {
 	//   baseUrl: '/test-site/',
 
 	// Used for publishing and more
-	projectName: 'docusaurus',
+	projectName: 'portal',
 	organizationName: 'baby-degu',
 	// For top-level user or org sites, the organization is still the same.
 	// e.g., for the https://JoelMarcey.github.io site, it would be set like...
